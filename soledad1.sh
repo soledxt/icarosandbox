2971c5534a3d97248bda03535fde33328113751e
soledad <soledixit@gmail.com>
2021-02-18 19:56:26 -0300

    este es el segundo ejericio con commit

---
+++ soledad1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+string_a="Linux"
+string_b="GENIAL"
+
+echo "Son $string_a y $string_b el mismo string?"
+[ $string_a = $string_b ]
+echo $?
+
+
+num_a=100
+num_b=100
+
+echo "Es $num_a igual a $num_b ?"
+[ $num_a -eq $num_b ]
+echo $?
+
+
